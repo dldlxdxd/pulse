64acbe521e1386253ebbef15bc7c7d1b8443b463
Daria <dbarsova88@gmail.com>
2022-08-06 16:27:29 +0300

    add slick slider

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        speed: 1200,
+        //adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }   
+        ]
+      });
+  });
+  
